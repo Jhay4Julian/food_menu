0a86d6f5c18c51602a09985b3e21b1baa605509e
Julian Koripamo <jkoripamo@gmail.com>
2024-08-03 21:33:03 +0100

    designed the page title, added the json menu list and implemented the food card in the listview builder.

--- lib/menu_page.dart
+++ lib/menu_page.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:food_menu/components/category_button.dart';
+import 'package:food_menu/components/food_card.dart';
 
 class MenuPage extends StatefulWidget {
   const MenuPage({super.key});
@@ -7,9 +9,155 @@ class MenuPage extends StatefulWidget {
   State<MenuPage> createState() => _MenuPageState();
 }
 
+List<Map<String, dynamic>> _menuList = [
+  {
+    "name": "Buttermilk Pancakes",
+    "price": "15.99",
+    "description":
+        "Delicious buttermilk pancakes served with butter and syrup. A perfect start to your day.",
+    "image": "assets/images/buttermilk_pancake.jpeg",
+    "type": "breakfast",
+  },
+  {
+    "name": "Country Delight",
+    "price": "20.99",
+    "description":
+        "A delightful country breakfast with scrambled eggs, sausage, and biscuits.",
+    "image": "assets/images/country_delight.jpeg",
+    "type": "breakfast",
+  },
+  {
+    "name": "Bacon Overflow",
+    "price": "8.99",
+    "description":
+        "Crispy bacon served with eggs and toast. A savory start to your day.",
+    "image": "assets/images/bacon_overflow.jpeg",
+    "type": "breakfast",
+  },
+  {
+    "name": "Egg Attack",
+    "price": "22.99",
+    "description":
+        "A protein-packed lunch with a perfectly cooked egg on a juicy beef patty.",
+    "image": "assets/images/egg_attack.jpeg",
+    "type": "lunch",
+  },
+  {
+    "name": "American Classic",
+    "price": "12.99",
+    "description":
+        "The all-American classic burger with cheese, lettuce, and tomato.",
+    "image": "assets/images/american_classic.jpeg",
+    "type": "lunch",
+  },
+  {
+    "name": "Diner Double",
+    "price": "13.99",
+    "description":
+        "Classic diner double with eggs, bacon, and hash browns. A hearty lunch to fuel your afternoon.",
+    "image": "assets/images/diner_double.jpeg",
+    "type": "lunch",
+  },
+  {
+    "name": "Oreo Dream",
+    "price": "18.99",
+    "description":
+        "A dreamy Oreo milkshake that's rich, creamy, and loaded with Oreo cookie bits.",
+    "image": "assets/images/oreo_dream.jpeg",
+    "type": "drinks",
+  },
+  {
+    "name": "Quarantine Buddy",
+    "price": "16.99",
+    "description":
+        "A comforting blend of flavors to keep you company, with hints of vanilla and chocolate.",
+    "image": "assets/images/quarantine_buddy.jpeg",
+    "type": "drinks",
+  },
+  {
+    "name": "Godzilla Milkshake",
+    "price": "6.99",
+    "description":
+        "An enormous milkshake packed with flavors and topped with whipped cream and cherries.",
+    "image": "assets/images/godzilla_milkshake.jpeg",
+    "type": "drinks",
+  }
+];
+
 class _MenuPageState extends State<MenuPage> {
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: SafeArea(
+        child: Column(
+          children: [
+            const SizedBox(height: 20),
+            Padding(
+              padding: const EdgeInsets.all(10.0),
+              child: Column(
+                children: [
+                  const Text(
+                    'Our Menu',
+                    style: TextStyle(
+                      fontSize: 33,
+                      fontWeight: FontWeight.w400,
+                      letterSpacing: 1,
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(top: 7),
+                    child: Container(
+                      height: 4,
+                      width: 90,
+                      color: const Color(0xFFF59E0B),
+                    ),
+                  )
+                ],
+              ),
+            ),
+            const SizedBox(height: 25),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 20),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  CategoryButton(
+                    title: 'All',
+                    onTap: () {},
+                  ),
+                  CategoryButton(
+                    title: 'Breakfast',
+                    onTap: () {},
+                  ),
+                  CategoryButton(
+                    title: 'Lunch',
+                    onTap: () {},
+                  ),
+                  CategoryButton(
+                    title: 'Shakes',
+                    onTap: () {},
+                  ),
+                ],
+              ),
+            ),
+            const SizedBox(height: 30),
+            Expanded(
+              child: ListView.builder(
+                itemCount: _menuList.length,
+                itemBuilder: (BuildContext context, index) {
+                  return FoodCard(
+                    imagePath: _menuList[index]['image'],
+                    name: _menuList[index]['name'],
+                    price: _menuList[index]['price'],
+                    description: _menuList[index]['description'],
+                  );
+                },
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
