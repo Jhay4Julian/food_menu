8e39107f38951065593844e2ef777ec1acb2d910
Julian Koripamo <jkoripamo@gmail.com>
2024-08-04 03:01:08 +0100

    restructured json data

--- lib/menu_page.dart
+++ lib/menu_page.dart
@@ -9,80 +9,79 @@ class MenuPage extends StatefulWidget {
   State<MenuPage> createState() => _MenuPageState();
 }
 
-List<Map<String, dynamic>> _menuList = [
+const String jsonData = '''
   {
-    "name": "Buttermilk Pancakes",
-    "price": "15.99",
-    "description":
-        "Delicious buttermilk pancakes served with butter and syrup. A perfect start to your day.",
-    "image": "assets/images/buttermilk_pancake.jpeg",
-    "type": "breakfast",
-  },
-  {
-    "name": "Country Delight",
-    "price": "20.99",
-    "description":
-        "A delightful country breakfast with scrambled eggs, sausage, and biscuits.",
-    "image": "assets/images/country_delight.jpeg",
-    "type": "breakfast",
-  },
-  {
-    "name": "Bacon Overflow",
-    "price": "8.99",
-    "description":
-        "Crispy bacon served with eggs and toast. A savory start to your day.",
-    "image": "assets/images/bacon_overflow.jpeg",
-    "type": "breakfast",
-  },
-  {
-    "name": "Egg Attack",
-    "price": "22.99",
-    "description":
-        "A protein-packed lunch with a perfectly cooked egg on a juicy beef patty.",
-    "image": "assets/images/egg_attack.jpeg",
-    "type": "lunch",
-  },
-  {
-    "name": "American Classic",
-    "price": "12.99",
-    "description":
-        "The all-American classic burger with cheese, lettuce, and tomato.",
-    "image": "assets/images/american_classic.jpeg",
-    "type": "lunch",
-  },
-  {
-    "name": "Diner Double",
-    "price": "13.99",
-    "description":
-        "Classic diner double with eggs, bacon, and hash browns. A hearty lunch to fuel your afternoon.",
-    "image": "assets/images/diner_double.jpeg",
-    "type": "lunch",
-  },
-  {
-    "name": "Oreo Dream",
-    "price": "18.99",
-    "description":
-        "A dreamy Oreo milkshake that's rich, creamy, and loaded with Oreo cookie bits.",
-    "image": "assets/images/oreo_dream.jpeg",
-    "type": "drinks",
-  },
-  {
-    "name": "Quarantine Buddy",
-    "price": "16.99",
-    "description":
-        "A comforting blend of flavors to keep you company, with hints of vanilla and chocolate.",
-    "image": "assets/images/quarantine_buddy.jpeg",
-    "type": "drinks",
-  },
-  {
-    "name": "Godzilla Milkshake",
-    "price": "6.99",
-    "description":
-        "An enormous milkshake packed with flavors and topped with whipped cream and cherries.",
-    "image": "assets/images/godzilla_milkshake.jpeg",
-    "type": "drinks",
+    "breakfast": [
+      {
+        "name": "Buttermilk Pancakes",
+        "price": "15.99",
+        "description":
+            "Delicious buttermilk pancakes served with butter and syrup. A perfect start to your day.",
+        "image": "assets/images/buttermilk_pancake.jpeg",
+      },
+      {
+        "name": "Country Delight",
+        "price": "20.99",
+        "description":
+            "A delightful country breakfast with scrambled eggs, sausage, and biscuits.",
+        "image": "assets/images/country_delight.jpeg",
+      },
+      {
+        "name": "Bacon Overflow",
+        "price": "8.99",
+        "description":
+            "Crispy bacon served with eggs and toast. A savory start to your day.",
+        "image": "assets/images/bacon_overflow.jpeg",
+      },
+    ],
+    "lunch": [
+      {
+        "name": "Egg Attack",
+        "price": "22.99",
+        "description":
+            "A protein-packed lunch with a perfectly cooked egg on a juicy beef patty.",
+        "image": "assets/images/egg_attack.jpeg",
+      },
+      {
+        "name": "American Classic",
+        "price": "12.99",
+        "description":
+            "The all-American classic burger with cheese, lettuce, and tomato.",
+        "image": "assets/images/american_classic.jpeg",
+      },
+      {
+        "name": "Diner Double",
+        "price": "13.99",
+        "description":
+            "Classic diner double with eggs, bacon, and hash browns. A hearty lunch to fuel your afternoon.",
+        "image": "assets/images/diner_double.jpeg",
+      },
+    ],
+    "drinks": [
+      {
+        "name": "Oreo Dream",
+        "price": "18.99",
+        "description":
+            "A dreamy Oreo milkshake that's rich, creamy, and loaded with Oreo cookie bits.",
+        "image": "assets/images/oreo_dream.jpeg",
+      },
+      {
+        "name": "Quarantine Buddy",
+        "price": "16.99",
+        "description":
+            "A comforting blend of flavors to keep you company, with hints of vanilla and chocolate.",
+        "image": "assets/images/quarantine_buddy.jpeg",
+      },
+      {
+        "name": "Godzilla Milkshake",
+        "price": "6.99",
+        "description":
+            "An enormous milkshake packed with flavors and topped with whipped cream and cherries.",
+        "image": "assets/images/godzilla_milkshake.jpeg",
+      }
+    ]
   }
-];
+''';
 
 class _MenuPageState extends State<MenuPage> {
   @override
@@ -142,19 +141,6 @@ class _MenuPageState extends State<MenuPage> {
               ),
             ),
             const SizedBox(height: 30),
-            Expanded(
-              child: ListView.builder(
-                itemCount: _menuList.length,
-                itemBuilder: (BuildContext context, index) {
-                  return FoodCard(
-                    imagePath: _menuList[index]['image'],
-                    name: _menuList[index]['name'],
-                    price: _menuList[index]['price'],
-                    description: _menuList[index]['description'],
-                  );
-                },
-              ),
-            ),
           ],
         ),
       ),
